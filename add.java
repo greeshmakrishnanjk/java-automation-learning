4b3a3da604d494dd1a81c37d0717f0c90e4f94bd
greeshmakrishnanjk <greeshmakrishnanjk@gmail.com>
2024-07-20 21:01:41 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,24 +2,8 @@ import java.util.*;
  
   public class add {
  
-     public static void main(String[] args)
+    public static void main(String[] args)
     {
-        int n = 10;
-        int i;
- 
-        Integer intArray[]
-            = { 1, 2, 3, 4, 5 };
- 
-        System.out.println("Original Array:\n" + Arrays.toString(intArray));
- 
-        int num = 10;
- 
-        List<Integer>intlist = new ArrayList<Integer>(Arrays.asList(intArray));
- 
-        intlist.add(num);
- 
-        intArray = intlist.toArray(intArray);
- 
-        System.out.println("\nArray after adding " + num + "\n" + Arrays.toString(intArray));
+    	int a = 4-2;
     }
 }
